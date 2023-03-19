dec75b1ffcaa1015585c8087f691b531ff092300
Sameer Ajmani <47604025+ajmanisameer@users.noreply.github.com>
2023-03-19 19:00:58 +0530

    Layout blueprint/ Sidebar icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-3xl text-sky-500">Heloo World</div>
+
+    <>
+    <Header label="Home"/>
+    </>
   )
 }
